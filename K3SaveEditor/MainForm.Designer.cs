6a4b5550b35e7d5e81e8a10f9380df3c92a06bdb
alextrack2013 <alextrack2013@gmail.com>
2025-05-11 13:38:53 +0200

    Adjust Devas panel size

--- K3SaveEditor/MainForm.Designer.cs
+++ K3SaveEditor/MainForm.Designer.cs
@@ -33,24 +33,24 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.checkBox7 = new System.Windows.Forms.CheckBox();
-            this.checkBox8 = new System.Windows.Forms.CheckBox();
-            this.checkBox9 = new System.Windows.Forms.CheckBox();
-            this.checkBox10 = new System.Windows.Forms.CheckBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -114,73 +114,28 @@
             this.panel1.Size = new System.Drawing.Size(776, 288);
             this.panel1.TabIndex = 4;
             // 
-            // label2
+            // label6
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Room";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Features";
             // 
-            // label3
+            // panel2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 30);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 20);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Player";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(7, 89);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(308, 24);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Unlock portal room (all bosses passed)\r\n";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(7, 149);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(124, 24);
-            this.checkBox2.TabIndex = 5;
-            this.checkBox2.Text = "Boss HP bar";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(7, 119);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(96, 24);
-            this.checkBox3.TabIndex = 6;
-            this.checkBox3.Text = "Autogun";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(7, 209);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(124, 24);
-            this.checkBox4.TabIndex = 7;
-            this.checkBox4.Text = "Crystal radar";
-            this.checkBox4.UseVisualStyleBackColor = true;
-            // 
-            // checkBox5
-            // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(7, 179);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(132, 24);
-            this.checkBox5.TabIndex = 8;
-            this.checkBox5.Text = "Boss time bar";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.checkBox10);
+            this.panel2.Controls.Add(this.checkBox7);
+            this.panel2.Controls.Add(this.checkBox9);
+            this.panel2.Controls.Add(this.checkBox8);
+            this.panel2.Location = new System.Drawing.Point(631, 62);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(142, 174);
+            this.panel2.TabIndex = 16;
             // 
             // label5
             // 
@@ -192,34 +147,15 @@
             this.label5.TabIndex = 10;
             this.label5.Text = "Stage 4 Devas";
             // 
-            // button3
+            // checkBox10
             // 
-            this.button3.Location = new System.Drawing.Point(672, 348);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 32);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Save";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // checkBox6
-            // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(7, 239);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(157, 24);
-            this.checkBox6.TabIndex = 11;
-            this.checkBox6.Text = "Max crystal count";
-            this.checkBox6.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 354);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(340, 20);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Make sure to do a backup before any changes!";
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(7, 117);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(82, 24);
+            this.checkBox10.TabIndex = 15;
+            this.checkBox10.Text = "Spade";
+            this.checkBox10.UseVisualStyleBackColor = true;
             // 
             // checkBox7
             // 
@@ -231,16 +167,6 @@
             this.checkBox7.Text = "Club";
             this.checkBox7.UseVisualStyleBackColor = true;
             // 
-            // checkBox8
-            // 
-            this.checkBox8.AutoSize = true;
-            this.checkBox8.Location = new System.Drawing.Point(7, 57);
-            this.checkBox8.Name = "checkBox8";
-            this.checkBox8.Size = new System.Drawing.Size(75, 24);
-            this.checkBox8.TabIndex = 13;
-            this.checkBox8.Text = "Heart";
-            this.checkBox8.UseVisualStyleBackColor = true;
-            // 
             // checkBox9
             // 
             this.checkBox9.AutoSize = true;
@@ -251,38 +177,112 @@
             this.checkBox9.Text = "Diamond";
             this.checkBox9.UseVisualStyleBackColor = true;
             // 
-            // checkBox10
+            // checkBox8
             // 
-            this.checkBox10.AutoSize = true;
-            this.checkBox10.Location = new System.Drawing.Point(7, 117);
-            this.checkBox10.Name = "checkBox10";
-            this.checkBox10.Size = new System.Drawing.Size(82, 24);
-            this.checkBox10.TabIndex = 15;
-            this.checkBox10.Text = "Spade";
-            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(7, 57);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(75, 24);
+            this.checkBox8.TabIndex = 13;
+            this.checkBox8.Text = "Heart";
+            this.checkBox8.UseVisualStyleBackColor = true;
             // 
-            // panel2
+            // checkBox6
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.checkBox10);
-            this.panel2.Controls.Add(this.checkBox7);
-            this.panel2.Controls.Add(this.checkBox9);
-            this.panel2.Controls.Add(this.checkBox8);
-            this.panel2.Location = new System.Drawing.Point(573, 62);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 174);
-            this.panel2.TabIndex = 16;
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(7, 239);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(157, 24);
+            this.checkBox6.TabIndex = 11;
+            this.checkBox6.Text = "Max crystal count";
+            this.checkBox6.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // checkBox5
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(3, 62);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(81, 20);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "Features";
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(7, 179);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(132, 24);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Boss time bar";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(7, 209);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(124, 24);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Crystal radar";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 119);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(96, 24);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Autogun";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 149);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(124, 24);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Boss HP bar";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(308, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Unlock portal room (all bosses passed)\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Room";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(672, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(340, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Make sure to do a backup before any changes!";
             // 
             // MainForm
             // 
